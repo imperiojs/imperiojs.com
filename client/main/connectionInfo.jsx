bc7f767265b1e8272f3b7cca9e1b74249d19b5f7
Austin <austin.lyon@gmail.com>
2016-07-20 13:10:19 -0700

    removed imperio listeners. Started experimenting with npm start

--- client/main/connectionInfo.jsx
+++ client/main/connectionInfo.jsx
@@ -12,22 +12,22 @@ const ConnectionInfo = React.createClass({
   render() {
     // TODO include connection type! (webRTC vs sockets)
     const connectionKeys = Object.keys(this.props.connections);
-    console.log('connectionKeys:', connectionKeys);
     let connections = [];
     if (connectionKeys.length > 0) {
-      console.log('length > 0!');
       for (const key of connectionKeys) {
         // if (this.props.connections[key] === 'listener') {
         //   connections += <p>{`${key} + ${this.props.connections[key]}`}</p>;
         // }
-        connections.push(<p key={key}>{key + this.props.connections[key]}</p>);
+        connections.push(<p>{key + ': ' + this.props.connections[key]}</p>);
       }
     }
-    console.log('connections', connections);
     return (
       <div id="connection-wrap">
         {connections}
-        <div id="connection">Connect: {imperio.nonce}</div>
+        <div id="connection">
+          {connections}
+          <p>Connect: {imperio.nonce}</p>
+        </div>
       </div>
     );
   },
