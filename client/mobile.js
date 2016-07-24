89519ce5ea34374c67027d14bc202661dba7d2b1
Matthew McLaughlin <Matthew McLaughlin>
2016-07-23 17:30:18 -0700

    more work on mobile

--- client/mobile.js
+++ client/mobile.js
@@ -28,7 +28,20 @@ imperio.dataListener(handleImperioEmitters);
 
 // turns on and off mobile emitters conditionally
 function handleImperioEmitters(actionObj) {
-  console.log(actionObj);
+  // ------ Iacto Emitters ------
+  if (actionObj.action === 'start_iacto') {
+    console.log('adding Iacto event listeners');
+    document.getElementById('iactoMobile').style.display = 'flex';
+    imperio.emitAcceleration.gravity();
+    imperio.emitGyroscope.start(handleGyro);
+  }
+  else if (actionObj.action === 'stop_iacto') {
+    console.log('removing Iacto listeners');
+    document.getElementById('iactoMobile').style.display = 'none';
+    imperio.emitAcceleration.removeGravity();
+    imperio.emitGyroscope.remove(handleGyro);
+  }
+
   if (actionObj && actionObj.hasOwnProperty('action')) {
     // ------ Umbra Emitters ------
     if (actionObj.action === 'start_umbra') {
@@ -43,21 +56,6 @@ function handleImperioEmitters(actionObj) {
       document.body.removeEventListener('touchend', emitNewZeroValues);
       imperio.emitGyroscope.remove(printGyroData);
     }
-
-    // ------ Umbra Emitters ------
-    if (actionObj.action === 'start_iacto') {
-      console.log('adding Iacto event listeners');
-      document.getElementById('iactoMobile').style.display = 'block';
-      imperio.emitAcceleration.gravity();
-      imperio.emitGyroscope.start(handleGyro);
-    }
-    else if (actionObj.action === 'stop_iacto') {
-      console.log('removing Iacto listeners');
-      document.getElementById('iactoMobile').style.display = 'none';
-      imperio.emitAcceleration.removeGravity();
-      imperio.emitGyroscope.remove(handleGyro);
-    }
-
   } else {
     console.log('no actionObj');
   }
